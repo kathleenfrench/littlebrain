8cfb8a4ab4552bbb85adb58350629673bcda0d06
kathleen french <kathleen.french@bounceexchange.com>
2019-05-26 13:56:59 -0400

    add emojis moods to extreme sentiments detected

--- littlebrain.py
+++ littlebrain.py
@@ -5,6 +5,16 @@ from pkg import helper, cmds
 from blessings import Terminal
 import time, webbrowser, datetime
 import requests
+import emoji
+
+import nltk
+from nltk.corpus import stopwords, wordnet
+from nltk.tokenize import word_tokenize
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+nltk.data.path.append('./nltk_data/')
 
 ## info
 from wikiapi import WikiApi
@@ -21,10 +31,18 @@ LB_FACTS = {
   "master": "kathleen, or you if you're cooler"
 }
 
+## sentiment scores
+MAX_NEGATIVE = "xxx"
+NEGATIVE = "xxy"
+NEUTRAL = "xyx"
+POSITIVE = "xyy"
+MAX_POSITIVE = "yxx"
+
 class LittleBrain():
   def __init__(self):
     self.t = Terminal()
     self.kernel = aiml.Kernel()
+    self.analyzer = SentimentIntensityAnalyzer()
     if os.path.isfile("bot_brain.brn"):
       self.kernel.bootstrap(brainFile="bot_brain.brn")
     else:
@@ -41,6 +59,8 @@ class LittleBrain():
     while True:
         message = input(">> ")
         split_message = message.split()
+
+        sentiment_score, mood = self.score_input_sentiment(message)
 
         if message == "quit" or message == "exit" or message == "bye" or message == "gtg" or message == "ok bye": # exit from chat
           print(self.t.blue("k bye..."))
@@ -61,7 +81,72 @@ class LittleBrain():
           cmds.Commands(split_message)
         else:
           bot_response = self.kernel.respond(message)
-          print(self.t.blue(bot_response).lower().strip())
+          # self.improve_bot_response(bot_response)
+
+          if mood is None:
+            print(self.t.blue(bot_response).lower().strip())
+          else:
+            print(mood, " ", self.t.blue(bot_response).lower().strip())
+
+  def score_input_sentiment(self, message):
+    sentiment_compound = self.analyzer.polarity_scores(message)['compound']
+    score = None
+    mood = None
+
+    if sentiment_compound <= -0.5:
+      score = MAX_NEGATIVE
+      mood = emoji.emojize(':confounded:', use_aliases=True)
+    elif sentiment_compound > -0.5 and sentiment_compound < 0:
+      score = NEGATIVE
+      mood = emoji.emojize(':cold_sweat:', use_aliases=True)
+    elif sentiment_compound == 0:
+      score = NEUTRAL
+    elif sentiment_compound > 0 and sentiment_compound < 0.5:
+      score = POSITIVE
+      mood = emoji.emojize(':grin:', use_aliases=True)
+    else:
+      score = MAX_POSITIVE
+      mood = emoji.emojize(':smile:', use_aliases=True)
+
+    return score, mood
+
+  def improve_bot_response(self, response):
+    dict = {}
+    sentence = response.lower().strip()
+    stop_words = set(stopwords.words('english'))
+    word_tokens = word_tokenize(sentence)
+    parsed_input = [wt for wt in word_tokens if not wt in stop_words]
+    tagged = nltk.pos_tag(parsed_input)
+    new_response = ""
+
+    for word in tagged:
+      try:
+        wtype = word[1]
+        if (dict[wtype] != None):
+          part_of_speech = dict[wtype]
+        else:
+          part_of_speech = 'NOUN'
+        
+        if part_of_speech == 'NOUN':
+          word_mod = wordnet.morphy(word[0], wordnet.NOUN)
+        elif part_of_speech == 'VERB':
+          word_mod = wordnet.morphy(word[0], wordnet.VERB)
+        elif part_of_speech == 'ADV':
+          word_mod = wordnet.morphy(word[0], wordnet.ADV)
+        elif part_of_speech == 'ADJ':
+          word_mod = wordnet.morphy(word[0], wordnet.ADJ)
+
+        updated_word = wordnet.synsets(word_mod)[0].lemmas()[0].name()
+
+      except:
+        updated_word = word[0]
+
+      if new_response == "":
+        new_response = new_response + updated_word.lower()
+      else:
+        new_response = new_response + " " + updated_word.lower()
+
+      print("NEW RESPONSE: ", new_response)
 
 
 if __name__ == '__main__':
