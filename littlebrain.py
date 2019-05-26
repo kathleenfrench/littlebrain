53e2dae1623e1351257f9133a15827c4e006278b
kathleen french <kathleen.french@bounceexchange.com>
2019-05-26 14:06:03 -0400

    cleanup

--- littlebrain.py
+++ littlebrain.py
@@ -28,15 +28,8 @@ LB_FACTS = {
   "party": "all night long",
   "size": "extremely tall",
   "looklike": "your biggest crush",
-  "master": "kathleen, or you if you're cooler"
+  "master": "kathleen, or you if you're cooler - i'm an independent lil brain"
 }
-
-## sentiment scores
-MAX_NEGATIVE = "xxx"
-NEGATIVE = "xxy"
-NEUTRAL = "xyx"
-POSITIVE = "xyy"
-MAX_POSITIVE = "yxx"
 
 class LittleBrain():
   def __init__(self):
@@ -67,16 +60,12 @@ class LittleBrain():
           exit()
         elif message == "save" or message == "!save":
           self.kernel.saveBrain("bot_brain.brn")
-        # if the message is a command
         elif message == "help":
           helper.Features()
-        # elif "where is" in message:
-        #   location = split_message[2]
-        #   print("let me show you where {} is...".format(location))
-        #   webbrowser.open("https://www.google.co.in/maps?q={0}".format(location))
         elif "what's today" in message:
           today = "today is {0}".format(datetime.datetime.utcnow().date().strftime('%Y-%m-%d'))
           print(today)
+        # if the message is a command
         elif message[:1] == "!":
           cmds.Commands(split_message)
         else:
@@ -90,64 +79,23 @@ class LittleBrain():
 
   def score_input_sentiment(self, message):
     sentiment_compound = self.analyzer.polarity_scores(message)['compound']
-    # score = None
     mood = None
 
     if sentiment_compound <= -0.5:
-      # score = MAX_NEGATIVE
       mood = emoji.emojize(':confounded:', use_aliases=True)
     elif sentiment_compound > -0.5 and sentiment_compound < 0:
-      # score = NEGATIVE
       mood = emoji.emojize(':cold_sweat:', use_aliases=True)
     elif sentiment_compound == 0:
       pass
-      # score = NEUTRAL
     elif sentiment_compound > 0 and sentiment_compound < 0.5:
-      # score = POSITIVE
       mood = emoji.emojize(':grin:', use_aliases=True)
     else:
-      # score = MAX_POSITIVE
       mood = emoji.emojize(':smile:', use_aliases=True)
 
     return mood
 
   def improve_bot_response(self, response):
-    dict = {}
-    sentence = response.lower().strip()
-    stop_words = set(stopwords.words('english'))
-    word_tokens = word_tokenize(sentence)
-    parsed_input = [wt for wt in word_tokens if not wt in stop_words]
-    tagged = nltk.pos_tag(parsed_input)
-    new_response = ""
-
-    for word in tagged:
-      try:
-        wtype = word[1]
-        if (dict[wtype] != None):
-          part_of_speech = dict[wtype]
-        else:
-          part_of_speech = 'NOUN'
-        
-        if part_of_speech == 'NOUN':
-          word_mod = wordnet.morphy(word[0], wordnet.NOUN)
-        elif part_of_speech == 'VERB':
-          word_mod = wordnet.morphy(word[0], wordnet.VERB)
-        elif part_of_speech == 'ADV':
-          word_mod = wordnet.morphy(word[0], wordnet.ADV)
-        elif part_of_speech == 'ADJ':
-          word_mod = wordnet.morphy(word[0], wordnet.ADJ)
-
-        updated_word = wordnet.synsets(word_mod)[0].lemmas()[0].name()
-
-      except:
-        updated_word = word[0]
-
-      if new_response == "":
-        new_response = new_response + updated_word.lower()
-      else:
-        new_response = new_response + " " + updated_word.lower()
-
-      print("NEW RESPONSE: ", new_response)
+    ## TODO
 
 
 if __name__ == '__main__':
